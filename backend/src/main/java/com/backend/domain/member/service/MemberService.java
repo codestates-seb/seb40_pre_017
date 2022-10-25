7f74e50de02393bc932bfaf4b034728cca41443b
Taekgil99 <pjwlover2005@naver.com>
2022-10-25 20:54:04 +0900

    Feat: 회원가입 구현
    로컬 테스트환경에서  H2 DB 테스트 완료
    Todo: 기본 프로필 이미지 추가

--- backend/src/main/java/com/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/backend/domain/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.backend.domain.member.service;
 
-
+import com.backend.domain.member.domain.Member;
+import com.backend.domain.member.dto.MemberDto;
+import com.backend.domain.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,22 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    public Member createMember(MemberDto memberDto) {
+        Member member = Member.builder()
+                .email(memberDto.getEmail())
+                .password(memberDto.getPassword())
+                .username(memberDto.getUsername())
+                // 초기 평판: 0
+                .reputation(0L)
+                // TODO: 프로필 이미지 추가
+                .profileImage("https://i.imgur.com/1ZQ3Z0u.png")
+                .build();
+
+        return memberRepository.save(member);
+    }
 
 
 }
