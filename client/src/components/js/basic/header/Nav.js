f140afa4abc0d28a67653c6e0e89ab4d96b9cf2b
shinker1002 <shinker1002@naver.com>
2022-10-25 23:20:12 +0900

    Feat: Header 필요 컴포넌트 추가

--- client/src/components/js/basic/header/Nav.js
+++ client/src/components/js/basic/header/Nav.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import '../../../css/basic/header/nav.scss'
+import React from 'react';
+import '../../../css/basic/header/nav.scss';
+import Hamburger from './Hamburger';
+import Logo from './Logo';
+import Snav from './Snav';
+import Navinput from './Navinput';
+
 
 export default function Nav() {
   return (
-    <nav className='nav'>nav</nav>
+    <nav className='nav'>
+      <Hamburger />
+      <Logo />
+      <Snav />
+      <Navinput/>
+      <button>1</button>
+    </nav>
   )
 }
