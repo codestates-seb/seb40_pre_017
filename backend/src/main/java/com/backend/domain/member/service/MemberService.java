210121d8bd977be83a8251485b6cf5a5c2dfd388
Taekgil99 <pjwlover2005@naver.com>
2022-10-25 20:53:30 +0900

    Feat: MemberService 생성

---
+++ backend/src/main/java/com/backend/domain/member/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.backend.domain.member.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MemberService {
+
+
+
+}
