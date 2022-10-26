e49d133f869902dab20953684c863d2fc3d0245f
Inssg <nightwalker0131@naver.com>
2022-10-26 17:47:23 +0900

    feat:답변 기능 추가

--- backend/src/main/java/com/backend/domain/answer/domain/Answer.java
+++ backend/src/main/java/com/backend/domain/answer/domain/Answer.java
@@ -1,14 +1,21 @@
 package com.backend.domain.answer.domain;
 
+import com.backend.domain.answer.dto.AnswerPatchDto;
+import com.backend.domain.answer.dto.AnswerResponseDto;
 import com.backend.domain.member.domain.Member;
 import com.backend.domain.question.domain.Question;
+import com.backend.global.Audit.Auditable;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-public class Answer {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Answer extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "answer_id", nullable = false)
@@ -27,5 +34,30 @@ public class Answer {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder
+    public Answer(String content, Boolean isAccepted) {
+        this.content = content;
+        this.isAccepted = isAccepted;
+    }
+
+    public AnswerResponseDto toResponseDto() {
+        return AnswerResponseDto.builder()
+                .answerId(id)
+                .build();
+    }
+
+    public void patch(AnswerPatchDto dto) {
+
+        if(dto.getContent() != null)
+            this.content = dto.getContent();
+    }
+
+    public void accept() {
+        this.isAccepted = true;
+    }
+    public void unAccept() {
+        this.isAccepted = false;
+    }
 
 }
---
+++ backend/src/main/java/com/backend/domain/answer/dto/AnswerResponseDto.java
@@ -0,0 +1,12 @@
+package com.backend.domain.answer.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class AnswerResponseDto {
+    private long answerId;
+//    private String content;
+//    private boolean isAccepted;
+}
