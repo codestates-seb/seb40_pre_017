531e73274b4fe7f5556f98f8f0c498e269d2ba4f
shinker1002 <shinker1002@naver.com>
2022-10-26 16:37:02 +0900

    Feat: navinput 디자인 구현

--- client/src/components/js/basic/header/Navinput.js
+++ client/src/components/js/basic/header/Navinput.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import '../../../css/basic/header/navinput.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 export default function Navinput() {
   return (
-    <div>Navinput</div>
+    <div className='navInputArea'>
+      <div className='navIcon'>
+        <FontAwesomeIcon className='hamburger' icon={faMagnifyingGlass} />
+      </div>
+      <input className='navInput' type="text" placeholder="Search…" />
+    </div>
   )
 }
