d7a403b9c4eb990e155a798309151f75aecb44eb
Taekgil99 <pjwlover2005@naver.com>
2022-10-27 21:15:59 +0900

    Feat: add RefreshToken Reposotiry

---
+++ backend/src/main/java/com/backend/domain/refreshtoken/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.backend.domain.refreshtoken.repository;
+
+import com.backend.domain.refreshtoken.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(Long key);
+}
