0373ad587f66aea4424e87acc8ed0f1fa3ceaae1
leeseongjun <thwn40@naver.com>
2022-10-28 02:56:02 +0900

    Feat : 질문 전체 조회 페이징 (검색 X, 필터 X, Newest 정렬 O)

--- backend/src/main/java/com/backend/domain/question/controller/QuestionController.java
+++ backend/src/main/java/com/backend/domain/question/controller/QuestionController.java
@@ -1,36 +1,54 @@
 package com.backend.domain.question.controller;
 
 import com.backend.domain.question.dto.request.QuestionCreate;
+import com.backend.domain.question.dto.request.QuestionSearch;
 import com.backend.domain.question.dto.request.QuestionUpdate;
+import com.backend.domain.question.dto.response.QuestionResponse;
 import com.backend.domain.question.service.QuestionService;
+import com.backend.global.dto.Response.MultiResponse;
+import com.backend.global.dto.request.PageRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class QuestionController {
 
     private final QuestionService questionService;
 
     @PostMapping("/questions")
-    public ResponseEntity<Long> createQuestion(@Valid @RequestBody QuestionCreate questionCreate) {
-        return ResponseEntity.ok(questionService.createQuestion(questionCreate));
+    public ResponseEntity<Long> create(@Valid @RequestBody QuestionCreate questionCreate) {
+        return ResponseEntity.ok(questionService.create(questionCreate));
+    }
+
+    @GetMapping("/questions")
+    public ResponseEntity<MultiResponse<?>> getList(PageRequest pageable, @ModelAttribute QuestionSearch questionSearch) {
+        log.info("page= {}",pageable.getPage());
+        log.info("offset = {}",pageable.getOffset());
+        log.info("size = {}",pageable.getSize());
+
+
+        return ResponseEntity.ok(questionService.getList(pageable,questionSearch));
     }
 
     @PatchMapping("questions/{id}")
-    public ResponseEntity<Long> updateQuestion(@PathVariable Long id, @Valid @RequestBody QuestionUpdate questionUpdate){
+    public ResponseEntity<Long> update(@PathVariable Long id, @Valid @RequestBody QuestionUpdate questionUpdate){
 
-        return ResponseEntity.ok(questionService.updateQuestion(id, questionUpdate));
+        return ResponseEntity.ok(questionService.update(id, questionUpdate));
     }
 
     @DeleteMapping("questions/{id}")
-    public ResponseEntity<Long> deleteQuestion(@PathVariable Long id){
+    public ResponseEntity<Long> delete(@PathVariable Long id){
 
 
-        return ResponseEntity.ok(questionService.deleteQuestion(id));
+        return ResponseEntity.ok(questionService.delete(id));
 
     }
 
