14a31f0efff7525d0648daaa3b5babf6fa9c9fd8
shinker1002 <shinker1002@naver.com>
2022-10-29 21:07:56 +0900

    Feat: 로그인 인풋 데이터 관리 함수 구현

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './login.scss';
 import { Link } from 'react-router-dom';
 import Googlebtn from '../components/js/user/common/Googlebtn';
@@ -10,6 +10,22 @@ import Button from '../components/js/user/common/Button';
 let content = ["Log in with Google", "Log in with Github", "Log in with Facebook"];
 
 export default function Login() {
+
+  const [data, setDate] = useState({});
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const formSubmit = (e) =>{
+    e.preventDefault()
+    console.log(data)
+    console.log("submit")
+  }
+
+  const onChangeInput = (e) => {
+    setDate({...data, [e.target.name] : e.target.value});
+  }
+
   return (
     <div className='loginPage'>
       <div className='loginPageInner'>
@@ -21,9 +37,9 @@ export default function Login() {
         <Facebookbtn content={content[2]} />
         
         <form className='loginForm' action="#">
-          <Input labelName="Email" inputId="email" inputType="email" />
-          <Input labelName="Password" inputId="password" inputType="password" />
-          <Button btnContent="Log in" />
+          <Input labelName="Email" inputId="email" inputType="email" name="email" onChangeInput={onChangeInput} />
+          <Input labelName="Password" inputId="password" inputType="password" name="password" onChangeInput={onChangeInput} />
+          <Button formSubmit={formSubmit} btnContent="Log in" />
         </form>
         <div className='loginLink'>
           Don’t have an account?
