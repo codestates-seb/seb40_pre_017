b2e21171d722b47909c4a3b01de6ee718e7d5e4f
shinker1002 <shinker1002@naver.com>
2022-10-30 00:30:18 +0900

    Feat: 회원가입, 로그인 유효성 검사

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,7 @@ import Githubbtn from '../components/js/user/common/Githubbtn';
 import Facebookbtn from '../components/js/user/common/Facebookbtn';
 import Input from '../components/js/user/common/Input';
 import Button from '../components/js/user/common/Button';
+import Inputerror from '../components/js/user/common/Inputerror';
 
 let content = ["Log in with Google", "Log in with Github", "Log in with Facebook"];
 
@@ -18,12 +19,37 @@ export default function Login() {
 
   const formSubmit = (e) =>{
     e.preventDefault()
-    console.log(data)
-    console.log("submit")
+
+    let error = false
+
+    if(data.email === undefined){
+      setEmailError(true);
+      error = true
+    }
+    if(data.password === undefined){
+      setPasswordError(true);
+      error = true
+    }
+
+    if(!error){
+      console.log('제출')
+    }
+
   }
 
   const onChangeInput = (e) => {
     setDate({...data, [e.target.name] : e.target.value});
+    console.log(emailError)
+    console.log(passwordError)
+    // 이메일 유효성 체크
+
+    // 이메일 유효성이 확인되면 폼에서 생성한 에러 메세지 삭제.
+    if(data.email !== undefined){
+      setEmailError(false);
+    }
+    if(data.password !== undefined){
+      setPasswordError(false);
+    }
   }
 
   return (
@@ -36,9 +62,13 @@ export default function Login() {
         <Githubbtn content={content[1]} />
         <Facebookbtn content={content[2]} />
         
-        <form className='loginForm' action="#">
+        <form className='loginForm'>
           <Input labelName="Email" inputId="email" inputType="email" name="email" onChangeInput={onChangeInput} />
+          {emailError && <Inputerror text="Email cannot be empty." />}
+
           <Input labelName="Password" inputId="password" inputType="password" name="password" onChangeInput={onChangeInput} />
+          {passwordError && <Inputerror text="Password cannot be empty." />}
+     
           <Button formSubmit={formSubmit} btnContent="Log in" />
         </form>
         <div className='loginLink'>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,10 +8,14 @@ import Githubbtn from '../components/js/user/common/Githubbtn';
 import Facebookbtn from '../components/js/user/common/Facebookbtn';
 import Input from '../components/js/user/common/Input';
 import Button from '../components/js/user/common/Button';
+import Inputerror from '../components/js/user/common/Inputerror';
+
 
 let content = ["Log in with Google", "Log in with Github", "Log in with Facebook"];
-
-
+// 이메일 형식이여야 함.
+let emailExptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+//  비밀번호에 문자, 숫자, 특수문자가 각각 최소 1개 이상은 들어있어야 되고, 최소 8자리에서 최대 16자리.
+let passwordExptext = /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,16}$/;
 
 
 export default function Signup() {
@@ -30,14 +34,39 @@ export default function Signup() {
 
   const formSubmit = (e) =>{
     e.preventDefault()
-    console.log(data)
-    console.log("submit")
+
+    let error = false
+
+    if(!emailExptext.test(data.email)){
+      setEmailError(true);
+      error = true;
+    }
+    if(!passwordExptext.test(data.password)){
+      setPasswordError(true);
+      error = true;
+    }
+
+    if(!error){
+      console.log('제출')
+    }
+    
   }
 
   const onChangeInput = (e) => {
     setDate({...data, [e.target.name] : e.target.value});
+    console.log(emailError)
+    console.log(passwordError)
+    // 이메일 유효성 체크
+
+    // 이메일 유효성이 확인되면 폼에서 생성한 에러 메세지 삭제.
+    if(emailExptext.test(data.email)){
+      setEmailError(false);
+    }
+    if(passwordExptext.test(data.password)){
+      setPasswordError(false);
+    }
   }
-  
+
   return (
     <div className='signupPage'>
       <div className='flexBox'>
@@ -74,7 +103,11 @@ export default function Signup() {
           <form className='signupForm'>
             <Input labelName="Display name" inputId="text" inputType="text" name="name" onChangeInput={onChangeInput} />
             <Input labelName="Email" inputId="email" inputType="email" name="email" onChangeInput={onChangeInput} />
+            {emailError && <Inputerror text="The email is not a valid email address." />}
+
             <Input labelName="Password" inputId="password" inputType="password" name="password" onChangeInput={onChangeInput} />
+            {passwordError && <Inputerror text="The password is not a valid password." />}
+
             <p>Passwords must contain at least eight characters, including at least 1 letter and 1 number.</p>
             <div className='checkBot'></div>
             <Button formSubmit={formSubmit} btnContent="Sign up" />
