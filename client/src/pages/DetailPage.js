a6d0fec064f761b03c729f0b4546d25dc9dc1802
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-11-04 00:22:43 +0900

    Feat: useEffect 리팩토링 실패

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,17 +1,37 @@
 import AnswerList from '../components/js/answer/AnswerList'
 import QuestionDetail from '../components/js/question/QuestionDetail'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import Aside from '../components/js/aside/Aside';
 import Category from '../components/js/category/Category';
 
 import './DetailPage.scss'
 import { Link } from 'react-router-dom'
 import createdAt from '../components/js/createdAt/CreatedAt';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function DetailPage({accessToken}) {
+  let params = useParams();
 
   const location = useLocation();
-  const { item } = location.state;
+  // const { item } = location.state;
+
+  const [item, setItem] = useState();
+  
+    console.log(item)
+    
+  useEffect( ()=>{
+const getPosts = async () => {
+    const posts = await axios.get(`/api/questions/${params.id}`, {
+      headers: {
+        "ngrok-skip-browser-warning": "69420"
+        }
+      });
+      setItem(posts.data)
+    };
+
+    getPosts();
+  }, []);
 
   return (
     <div className='detailPageWrap'>
